3e562ad0c21c215a2ad9783cb6518d026272a31f
jesseash <45877873+jesseash@users.noreply.github.com>
2026-07-21 13:04:39 -0400

    Stop tracking ignored files

--- scripts/contamination-marker-injector.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%~dp0contamination-marker-injector.ps1" %*
--- scripts/distro-contamination-detector.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%~dp0distro-contamination-detector.ps1" %*
